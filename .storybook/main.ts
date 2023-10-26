23233573b28d6b94d90ea84c09161640bbb92767
iana <filonenko.jana@gmail.com>
2023-10-26 11:54:02 +0200

    chore: update storybook and node

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/vue3-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+export default config
