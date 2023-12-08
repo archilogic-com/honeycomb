b9b7be52de61c0c782c995cef99d2dc67d0f1133
iana <filonenko.jana@gmail.com>
2023-12-08 13:47:41 +0100

    tooltip docs

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue'
-import { useFloating, offset, flip, shift, autoPlacement } from '@floating-ui/vue'
+import { defineComponent, PropType, ref, computed } from 'vue'
+import { useFloating, arrow, flip, autoUpdate, shift } from '@floating-ui/vue'
 
 let id = 0
 
@@ -11,15 +11,17 @@ const createId = () => {
 export default defineComponent({
   name: 'ATooltip',
   props: {
+    /** determines how tooltip will be associated with the control that triggered it,
+     * i.e. with `aria-describedby` or `aria-labelledby` attribute.
+     */
     type: {
       type: String as PropType<'label' | 'description'>,
       default: 'label'
     },
+    /** The text content of the tooltip.
+     * Tooltips can span multiple lines, but try to keep it short.
+     */
     content: {
-      type: String,
-      default: ''
-    },
-    shortcut: {
       type: String,
       default: ''
     }
@@ -27,9 +29,15 @@ export default defineComponent({
   setup(props) {
     const reference = ref(null)
     const floating = ref(null)
-    const { floatingStyles } = useFloating(reference, floating, {
+    const floatingArrow = ref(null)
+    const { floatingStyles, middlewareData, placement } = useFloating(reference, floating, {
       placement: 'top',
-      middleware: [flip(), shift()]
+      whileElementsMounted: autoUpdate,
+      middleware: [
+        flip({ fallbackAxisSideDirection: 'start' }),
+        shift(),
+        arrow({ element: floatingArrow })
+      ]
     })
 
     const isVisible = ref(false)
@@ -48,15 +56,38 @@ export default defineComponent({
       [props.type === 'label' ? 'aria-labelledby' : 'aria-describedby']: tooltipId
     }
 
+    // arrow positioning https://floating-ui.com/docs/arrow#visualization
+    // followed instructions here https://codesandbox.io/p/sandbox/mystifying-kare-ee3hmh?file=%2Fsrc%2Findex.js
+    const side = computed(() => placement.value.split('-')[0])
+
+    const staticSide = computed(
+      () =>
+        ({
+          top: 'bottom',
+          right: 'left',
+          bottom: 'top',
+          left: 'right'
+        })[side.value]
+    )
+
+    const arrowStyle = computed(() => ({
+      left: middlewareData.value.arrow?.x != null ? `${middlewareData.value.arrow.x}px` : '',
+      top: middlewareData.value.arrow?.y != null ? `${middlewareData.value.arrow.y}px` : '',
+      [staticSide.value as string]: '4.5px'
+    }))
+
     return {
       reference,
       floating,
+      floatingArrow,
       floatingStyles,
+      middlewareData,
       isVisible,
       showTooltip,
       hideTooltip,
       tooltipId,
-      ariaSlotProps
+      ariaSlotProps,
+      arrowStyle
     }
   }
 })
@@ -68,17 +99,25 @@ export default defineComponent({
     @focusin="showTooltip"
     @mouseenter="showTooltip"
     @mouseleave="hideTooltip"
-    @focusout="hideTooltip">
+    @focusout="hideTooltip"
+    @keydown.esc="hideTooltip">
+    <!-- @slot the control that triggers the tooltip goes into the default slot. 
+    -->
     <slot v-bind="ariaSlotProps" />
     <div
       :id="tooltipId"
       ref="floating"
+      role="tooltip"
       :style="floatingStyles"
-      class="absolute top-0 left-0 max-w-[140px] min-w-10"
+      class="absolute top-0 left-0 max-w-[160px] min-w-10 z-10"
       :class="isVisible ? 'visible' : 'invisible'">
-      <div class="body-sm text-stone rounded shadow-sm bg-white p-2 m-2">
-        <span v-if="content">{{ content }}</span>
-        <span v-if="shortcut">{{ shortcut }}</span>
+      <div
+        ref="floatingArrow"
+        class="absolute w-2 h-2 bg-white rotate-45"
+        :style="arrowStyle"></div>
+      <div
+        class="flex items-center justify-center body-sm text-stone rounded shadow-sm bg-white py-1 px-2 m-2 gap-1">
+        <span v-if="content" class="py-[2px]">{{ content }}</span>
       </div>
     </div>
   </div>
--- src/stories/Tooltip.stories.ts
+++ src/stories/Tooltip.stories.ts
@@ -1,9 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import type { ComponentProps } from 'vue-component-type-helpers'
 import { ATooltip, AButton } from '../components'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
 
 type TooltipProps = ComponentProps<typeof ATooltip>
 
+/**
+ * A tooltip component provides extra information about a control than triggers it.
+ * It serves as an accessible label or description of a given control (see `type` prop).
+ *
+ * Only interactive elements should trigger tooltips,
+ * and the tooltip itself cannot contain any interactive content.
+ *
+ * Do not put any essential information inside a tooltip,
+ * and always assume some users (e.g. on touch devices) will never see it.
+ *
+ * The tooltips are shown on mouseenter and focus events, and hidden on mouseleave and blur events,
+ * or with an ESC key while the control is focused.
+ *
+ * Tooltips are positioned automatically and re-positioned if the content is scrolled,
+ * taking into account available space (handled by [Floating UI](https://floating-ui.com/docs/getting-started)).
+ */
 const meta: Meta<TooltipProps> = {
   component: ATooltip,
   title: 'Components/Tooltip',
@@ -27,24 +44,53 @@ const renderTooltip = (args: ComponentProps<TooltipProps>) => ({
   </ATooltip>`
 })
 
+const hoverButton = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  const canvas = within(canvasElement)
+  const button = canvas.getByRole('button')
+  await waitFor(async () => {
+    await userEvent.hover(button)
+  })
+}
+
+/**
+ * Below are examples of differently positioned tooltips
+ * based on available space on various sides of the screen.
+ *
+ * The positioning logic is handled by [Floating UI](https://floating-ui.com/docs/vue#options) library,
+ * with the use of `flip()` and `shift()` [middleware](https://floating-ui.com/docs/middleware).
+ *
+ * If there's available space on every side of the tooltip,
+ * it is rendered on the top center of the
+ */
 export const Primary: Story = {
+  decorators: [
+    () => ({
+      template: `<div class="w-full h-[200px] flex items-center justify-center"><story/></div>`
+    })
+  ],
+  render: renderTooltip,
+  play: hoverButton
+}
+
+export const Bottom: Story = {
+  ...Primary,
   decorators: [
     () => ({
       template: `<div class="w-full h-[200px] flex items-start justify-center"><story/></div>`
     })
-  ],
-  render: renderTooltip
+  ]
 }
-export const Bottom: Story = {
+
+export const Top: Story = {
+  ...Primary,
   decorators: [
     () => ({
       template: `<div class="w-full h-[200px] flex items-end justify-center"><story/></div>`
     })
-  ],
-  render: renderTooltip
+  ]
 }
 
-export const Left: Story = {
+export const Right: Story = {
   decorators: [
     () => ({
       template: `<div class="w-full h-[200px] flex items-center justify-start"><story/></div>`
@@ -53,18 +99,22 @@ export const Left: Story = {
   render: renderTooltip
 }
 
-export const Right: Story = {
+export const Left: Story = {
+  ...Primary,
   decorators: [
     () => ({
-      template: `<div class="w-full h-[200px] flex items-start justify-end"><story/></div>`
+      template: `<div class="w-full h-[200px] flex items-center justify-end"><story/></div>`
     })
-  ],
-  render: renderTooltip
+  ]
 }
 
-export const WithShortcut: Story = {
-  render: renderTooltip,
+/**
+ * Multiline content should run no more than 4 lines.
+ * Tooltip's max width is limited to 160px.
+ */
+export const Multiline: Story = {
+  ...Primary,
   args: {
-    shortcut: 'K'
+    content: 'Tooltip content can span over multiple lines'
   }
 }
